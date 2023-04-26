a3b36664076001bbc0cd2571c74cb648abe7f45d
___krimer___ <karleneg@student.21-school.ru>
2023-04-26 17:07:19 +0300

    Second path is ready

---
+++ build/CMakeFiles/M_Tisbi_Cursach.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/M_Tisbi_Cursach.dir/main.cpp.obj"
+  "CMakeFiles/M_Tisbi_Cursach.dir/main.cpp.obj.d"
+  "M_Tisbi_Cursach.exe"
+  "M_Tisbi_Cursach.exe.manifest"
+  "M_Tisbi_Cursach.pdb"
+  "libM_Tisbi_Cursach.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/M_Tisbi_Cursach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/M_Tisbi_Cursach.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for M_Tisbi_Cursach.
+# This may be replaced when dependencies are built.
