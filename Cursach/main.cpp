df7d2c0188ede131d80a882c2e15a5a411f78f4d
___krimer___ <karleneg@student.21-school.ru>
2023-06-13 00:07:48 +0300

    Finish

--- Cursach/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Tree/Tree.h"
-
-int main() {
-    Tree<std::string> tree;
-    tree.insert("A");
-    return 0;
-}
--- build/CMakeFiles/M_Tisbi_Cursach.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for M_Tisbi_Cursach.
-# This may be replaced when dependencies are built.
